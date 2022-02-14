ab4c4c6c54967bf4a15853f72c08062d6ab422b4
JazzyLucas <lucasrendell@rocketmail.com>
2022-02-14 16:37:08 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+/**
+ * COP4610: Programming Project 1
+ * <p>
+ * Write a program in Java which creates producer and consumer threads. The
+ * producer should “produce” by setting the  elements of an array of integers to
+ * FULL. The consumer should “consume” by setting the elements of an array of
+ * integers to EMPTY.
+ *
+ * @author Lucas Rendell
+ * @version 2/17/2022
+ */
+
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
