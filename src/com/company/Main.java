376455d107e6f06f85df2eed05741d2493f09fe5
JazzyLucas <lucasrendell@rocketmail.com>
2022-02-14 20:01:04 -0500

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,5 @@
+package com.company;
+
 /**
  * COP4610: Programming Project 1
  * <p>
@@ -10,15 +12,97 @@
  * @version 2/17/2022
  */
 
-package com.company;
-
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+    public static void main(String[] args) throws InterruptedException {
+
+        ProducerConsumer pc = new ProducerConsumer();
+
+        // Threads
+        Thread producerThread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    pc.Produce();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread consumerThread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    pc.Consume();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Starts
+        producerThread.start();
+        consumerThread.start();
+
+        // Joins
+        producerThread.join();
+        consumerThread.join();
+
+    }//end main()
+}//end Main
+
+class ProducerConsumer extends Thread {
+
+    LinkedList<Integer> sharedData = new LinkedList<Integer>();
+    int capacity = 10;
+
+    public void Produce() throws InterruptedException
+    {
+        int data = 0;
+        while (true) {
+            synchronized (this)
+            {
+                while (sharedData.size() == capacity)
+                    wait();
+
+                data+=1;
+                sharedData.add(data);
+                System.out.println("Producer added: " + data);
+
+                notify();
+
+                // Delay
+                Thread.sleep(100);
+            }
+        }
+    }//end Produce()
+
+    public void Consume() throws InterruptedException
+    {
+        while (true) {
+            synchronized (this)
+            {
+                // consumer thread waits while list
+                // is empty
+                while (sharedData.size() == 0)
+                    wait();
+
+                int data = sharedData.remove(0);
+                System.out.println("Consumer removed: " + data);
+
+                notify();
+
+                // Delay
+                Thread.sleep(100);
+            }
+        }
+    }//end Consume()
+}//end ProducerConsumer
